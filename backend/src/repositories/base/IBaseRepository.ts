9cdc6b6088eb28fb83c93bcbcb68db6d9fb0d601
KiranNaikS12 <kiransnaik56@gmail.com>
2025-01-10 01:11:04 +0530

    feat: Added base repository in repository layer

---
+++ backend/src/repositories/base/IBaseRepository.ts
@@ -0,0 +1,13 @@
+import { Model, Document, FilterQuery, PopulateOptions } from "mongoose";
+
+export interface IBaseRepository<T extends Document> {
+    create(data: Partial<T>): Promise<T>;
+    findById(id: string) : Promise<T | null>;
+    findOne(filter: FilterQuery<T>) : Promise<T | null>;
+    findOneWithPopulate(filter: FilterQuery<T>, populateOptions: PopulateOptions | PopulateOptions[]) : Promise<T | null>
+    find(filter: FilterQuery<T>) : Promise<T[]>;
+    update(id: string, data: Partial<T> | Partial<T>) : Promise<T | null>;
+    delete(id: string) : Promise<boolean>
+    setModel(newModel: Model<T>): void;
+    updateMany(filter: Record<string, any>, update: Record<string, any>) : Promise<any>
+}
