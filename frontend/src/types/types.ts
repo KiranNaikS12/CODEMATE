f597b5b4f753bdb91da38d0f553cac06aa30509b
KiranNaikS12 <kiransnaik56@gmail.com>
2025-01-14 01:50:49 +0530

    feat: Added common type file

---
+++ frontend/src/types/types.ts
@@ -0,0 +1,100 @@
+
+
+export type APIError = {
+    data?: {
+        message?: string;
+    };
+};
+
+export enum Role {
+    User = 'user',
+    Tutor = 'tutor',
+    Admin = 'admin'
+}
+
+export type RegisterFormData = {
+    username:string,
+    roleId:Role.Tutor,
+    email:string,
+    password:string,
+    confirmPassword:string
+}
+
+export type LoginFormData = {
+    email:string,
+    password:string
+}
+
+export type ResetPasswordFormData = {
+    password: string,
+    confirmPassword: string;
+}
+
+//ResponseType
+export interface ApiResponse<T>  {
+    success:boolean,
+    message:string,
+    data: T;
+    total?: number,
+    errorCode?: string | null;
+}
+
+
+export interface ProfileImageProps {
+    handleProfile: () => void;
+    userId: string;
+    roleId: string;
+    refetch: () => void;
+}
+
+// problem related types
+export type TestCaseInput = {
+    name: string;
+    value: string;
+}
+
+
+export type TestCase = {
+  inputs: TestCaseInput[],
+  output:string
+}
+
+export type Hint = {
+    content: string
+}
+
+export type Example = {
+    heading: string;
+    inputs: TestCaseInput[]
+    output: string;
+    explanation: string;
+};
+
+export type ProblemFormFields = {
+    id?: string,
+    slno: number;
+    title: string;
+    description: string;
+    difficulty: string;
+    tags: string[];
+    testCases: TestCase[];
+    hints: Hint[];
+    examples: Example[];
+};
+
+export type TableRow = (string | number | JSX.Element)[];
+
+export type CustomTableRow = {
+    id: string,
+    columns: (string | number | JSX.Element)[];
+}
+
+
+export interface ErrorData {
+    status: number;
+    data: {
+      message: string;
+      success: boolean;
+      errorCode: string | null;
+    };
+  }
