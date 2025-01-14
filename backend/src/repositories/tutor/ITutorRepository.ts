cae137c933fcc289e0934d1dd08479c46a8efc4d
KiranNaikS12 <kiransnaik56@gmail.com>
2025-01-14 10:57:05 +0530

    feat: Added tutorRepository and it's interface

---
+++ backend/src/repositories/tutor/ITutorRepository.ts
@@ -0,0 +1,7 @@
+import { IBaseRepository } from "../base/IBaseRepository";
+import { ITutor } from "../../types/tutorTypes";
+
+
+export interface ITutorRepository extends IBaseRepository<ITutor> {
+    
+}
---
+++ backend/src/repositories/tutor/tutorRepository.ts
@@ -0,0 +1,17 @@
+import { Model, Document } from "mongoose";
+import { BaseRepository } from "../base/baseRepository";
+import { ITutor } from "../../types/tutorTypes";
+import {inject, injectable} from 'inversify';
+import { ITutorRepository } from "./ITutorRepository";
+
+
+
+@injectable()
+export class TutorRepository extends BaseRepository<ITutor> implements ITutorRepository{
+    constructor(
+       @inject('TutorModel') private tutorModel: Model<ITutor & Document>
+    ){
+        super(tutorModel)
+    }
+
+}
