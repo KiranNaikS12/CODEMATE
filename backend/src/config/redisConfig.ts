87ab0f5ebc726792b31ce8aba23f04fe5196fc6f
KiranNaikS12 <kiransnaik56@gmail.com>
2025-01-19 15:14:06 +0530

    fix: Made changes in redis

--- backend/src/config/redisConfig.ts
+++ backend/src/config/redisConfig.ts
@@ -4,6 +4,6 @@ import dotenv from 'dotenv';
 dotenv.config()
 
 export const redisConfig: IRedisConfig = {
-    host: process.env.REDIS_HOST || 'localhost',
+    host: process.env.REDIS_HOST || '127.0.0.1',
     port: parseInt(process.env.REDIS_PORT || '6379')
 }
